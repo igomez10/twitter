118fe0909b7cd4feb4580a4bc31a76f58f285e44
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2019-03-24 21:49:46 -0500

    folder structure and gitignore

--- app/server.go
+++ app/server.go
@@ -1,42 +1,66 @@
 package main
 
 import "fmt"
-import "net"
+import "github.com/jinzhu/gorm"
+import _ "github.com/jinzhu/gorm/dialects/postgres"
+import "log"
+
 import "net/http"
 
 type client struct {
-	db *net.Conn
+	DB *gorm.DB
 }
 
-var connection *net.Conn
+var connection *gorm.DB
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	c := client{}
+	conn := c.OpenConnection("postgres", "password", "postgres", "localhost", 5432)
+	c.DB = conn
+	defer c.DB.Close()
+
 	http.HandleFunc("/health", c.serveHealth)
-	http.HandleFunc("/user/", c.RegisterUser)
+	http.HandleFunc("/user/", c.RegisterUser).Put()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Closed server")
 	}
 }
 
-func openConnection() *net.Conn {
-	var pClient client
+func (c *client) OpenConnection(user, password, database, host string, port int) *gorm.DB {
 	if connection != nil {
 		return connection
-	} else {
-		// TODO open connection
-		// TODO assign connection to pClient
-		// TODO return pointer to pClient
-		pClient = client{db: &connection}
 	}
-	return &pClient
+	baseURL := "postgres://%s:%s@%s:%d/%s?sslmode=disable"
+	URL := fmt.Sprintf(baseURL, user, password, host, port, database)
+	db, err := gorm.Open("postgres", URL)
+	if err != nil {
+		log.Println("Could not connect to database", err)
+		return nil
+	}
+	connection = db
+	fmt.Println("Connected to DB ✅")
+	return db
+
 }
 
-func 
-
 func (c *client) RegisterUser(w http.ResponseWriter, r *http.Request) {
-	//sql.Open("mysql", dataSourceName string)
+	num := 5
+	if c.UserExists(num) {
+		fmt.Fprint(w, "CONFLICT")
+		return
+	}
+
+}
+
+func (c client) UserExists(ID int) bool {
+	var wantedUser User
+	c.DB.First(&wantedUser, "ID = ?", ID)
+	if wantedUser.ID != 0 {
+		return true
+	}
+	return false
 }
 
 func (c *client) serveHealth(w http.ResponseWriter, r *http.Request) {
